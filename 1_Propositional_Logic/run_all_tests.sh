9191a3ea5119f3590e2bb571d4fc19ee7ce42b0d
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-08-27 18:06:39 +0200

    Testing framework in a separate file.

--- 1_Propositional_Logic/run_all_tests.sh
+++ 1_Propositional_Logic/run_all_tests.sh
@@ -2,11 +2,11 @@
 
 # Usage: elpi ./run_all_tests.sh 
 
-echo "lib_test.elpi"      ; elpi lib_test.elpi -test
-echo "formula_test.elpi"  ; elpi formula_test.elpi -test
-echo "prop_test.elpi"     ; elpi prop_test.elpi -test
-echo "fol_test.elpi"      ; elpi fol_test.elpi -test
-echo "fol_gen.elpi"       ; elpi fol_gen.elpi -test
-echo "fol_bool_test.elpi" ; elpi fol_bool_test.elpi -test
-echo "fol_mod3_test.elpi" ; elpi fol_mod3_test.elpi -test
-echo "fol_modn_test.elpi" ; elpi fol_modn_test.elpi -test
+echo "lib_test.elpi"      ; elpi lib_test.elpi -test 2> /dev/null
+echo "formula_test.elpi"  ; elpi formula_test.elpi -test 2> /dev/null
+echo "prop_test.elpi"     ; elpi prop_test.elpi -test 2> /dev/null
+echo "fol_test.elpi"      ; elpi fol_test.elpi -test 2> /dev/null
+echo "fol_gen.elpi"       ; elpi fol_gen.elpi -test 2> /dev/null
+echo "fol_bool_test.elpi" ; elpi fol_bool_test.elpi -test 2> /dev/null
+echo "fol_mod3_test.elpi" ; elpi fol_mod3_test.elpi -test 2> /dev/null
+echo "fol_modn_test.elpi" ; elpi fol_modn_test.elpi -test 2> /dev/null
