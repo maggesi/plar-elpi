f60733c40f22e971389aa7db3ee497c53e0f9815
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-08-28 13:23:28 +0200

    Tests for skolemization.

--- 1_Propositional_Logic/run_all_tests.sh
+++ 1_Propositional_Logic/run_all_tests.sh
@@ -2,11 +2,12 @@
 
 # Usage: elpi ./run_all_tests.sh 
 
-echo -n "lib      \t" ; elpi lib_test.elpi -test 2> /dev/null
-echo -n "formula  \t" ; elpi formula_test.elpi -test 2> /dev/null
-echo -n "prop     \t" ; elpi prop_test.elpi -test 2> /dev/null
-echo -n "fol      \t" ; elpi fol_test.elpi -test 2> /dev/null
-echo -n "fol_gen  \t" ; elpi fol_gen.elpi -test 2> /dev/null
+echo -n "lib      \t" ; elpi lib_test.elpi      -test 2> /dev/null
+echo -n "formula  \t" ; elpi formula_test.elpi  -test 2> /dev/null
+echo -n "prop     \t" ; elpi prop_test.elpi     -test 2> /dev/null
+echo -n "fol      \t" ; elpi fol_test.elpi      -test 2> /dev/null
+echo -n "fol_gen  \t" ; elpi fol_gen.elpi       -test 2> /dev/null
 echo -n "fol_bool \t" ; elpi fol_bool_test.elpi -test 2> /dev/null
 echo -n "fol_mod3 \t" ; elpi fol_mod3_test.elpi -test 2> /dev/null
 echo -n "fol_modn \t" ; elpi fol_modn_test.elpi -test 2> /dev/null
+echo -n "skolem   \t" ; elpi skolem.elpi        -test 2> /dev/null
