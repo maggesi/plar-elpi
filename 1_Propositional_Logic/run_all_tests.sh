ba9f7a498a3e8c5f4c967f59636bc69b8802c4c9
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2020-01-08 23:53:16 +0100

    Test for skolemization in a separate file.

--- 1_Propositional_Logic/run_all_tests.sh
+++ 1_Propositional_Logic/run_all_tests.sh
@@ -10,4 +10,4 @@ echo -n "fol_gen  \t" ; elpi fol_gen.elpi       -test 2> /dev/null
 echo -n "fol_bool \t" ; elpi fol_bool_test.elpi -test 2> /dev/null
 echo -n "fol_mod3 \t" ; elpi fol_mod3_test.elpi -test 2> /dev/null
 echo -n "fol_modn \t" ; elpi fol_modn_test.elpi -test 2> /dev/null
-echo -n "skolem   \t" ; elpi skolem.elpi        -test 2> /dev/null
+echo -n "skolem   \t" ; elpi skolem_test.elpi   -test 2> /dev/null
